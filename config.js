35aeaefff8a5b441dc40a7dc750676a8560181a4
tysontiatia <58131918+tysontiatia@users.noreply.github.com>
2026-03-17 04:48:29 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  POOL_NAME: 'The MoneyShot Pool',
+  SEASON: '2026',
+  BIN_ID: '69b92c7bc3097a1dd531dd8b',
+  API_KEY: '$2a$10$dKwaKc8vzhnNZoX4jcfmpucIWIVZTkl8DsKvsPpBaVtUhXfUGbhp6',
+  BIN_URL: 'https://api.jsonbin.io/v3/b/69b92c7bc3097a1dd531dd8b',
+  ROUND_MULTIPLIERS: { R1:1, R2:2, S16:3, E8:4, FF:5, NCG:6 },
+  HIGHLIGHT_PLAYERS: ['Tyson Tiatia', 'Kristin Tiatia'],
+  REFRESH_MINUTES: 3,
+};
